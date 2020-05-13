a689321271b797240fb2baec201a01e89cbf72e2
Nima Rahnemoon <nrahnemoon@gmail.com>
2020-05-13 00:51:40 -0700

    Update web for real

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,19 +1,20 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "ebe9fa9ec1147b9a0ae9e898c80bba57",
-"/": "ebe9fa9ec1147b9a0ae9e898c80bba57",
+  "index.html": "246839d9157a8506d35fe5a3eb1385dc",
+"/": "246839d9157a8506d35fe5a3eb1385dc",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "2838286d322aa7910034b3cc7c1cff20",
+"assets/LICENSE": "496299102a19032d745532375dac5a6c",
 "assets/images/logo_bros.png": "a4959a69b8b6966604b48b6374eff4e0",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/AssetManifest.json": "491ee23db17a2745c0a9fc7802758573",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "276f31aaadb70d2637e6b04983fd1ca9",
+"favicon.ico": "43d9fcbd22a46dcb808a5787e4b9951b",
+"main.dart.js": "f2b1a68c236b165a419ae91692d74cd3",
 "manifest.json": "2bbc35fc99c905c33c5207c2ed41ed76",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1"
+"icons/favicon-32x32.png": "21210eb786e533a08e38f2978b253cde",
+"icons/apple-icon-180x180.png": "0f508471fdca3f1e0fac038e6d94b52d",
+"icons/favicon-16x16.png": "c4deeb37eeed133cf7cea77e5058771e"
 };
 
 self.addEventListener('activate', function (event) {
